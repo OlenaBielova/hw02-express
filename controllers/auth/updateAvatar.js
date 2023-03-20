53464548e8daf756c3fecae9b337939f08eaa7f9
OlenaBielova <belova.elena11@gmail.com>
2023-03-20 19:32:45 +0200

    hw-5

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -3,14 +3,18 @@ const fs = require("fs/promises");
 const { User } = require("../../models");
 
 
-const avatarDir = path.join(__dirname, "../", "public", "avatars");
+const avatarDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
     const { _id } = req.user;
-    const { path: tempUpload, originalName } = req.file;
-    const resultUpload = path.join(avatarDir, originalName);
+    console.log(req.file);
+    const { path: tempUpload, originalname } = req.file;
+    const filename = `${_id}_${originalname}`;
+    const resultUpload = path.join(avatarDir, filename);
+    console.log(resultUpload);
     await fs.rename(tempUpload, resultUpload);
-    const avatarURL = path.join("avatars", originalName);
+    const avatarURL = path.join("avatars", filename);
+
     await User.findByIdAndUpdate(_id, { avatarURL });
 
     res.json({
