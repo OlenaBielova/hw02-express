cb3df9673a974b5d84a89c79f130fa49aaf41490
OlenaBielova <belova.elena11@gmail.com>
2023-03-21 17:46:15 +0200

    hw-6

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,4 +1,6 @@
 const register = require("./register");
+const verifyEmail = require("./verifyEmail");
+const resendVerifyEmail = require("./resendVerifyEmail");
 const login = require("./login");
 const getCurrent = require("./getCurrent");
 const logout = require("./logout");
@@ -6,8 +8,10 @@ const updateAvatar = require("./updateAvatar");
 
 module.exports = {
     register,
+    verifyEmail,
+    resendVerifyEmail,
     login,
     getCurrent,
     logout,
-    updateAvatar
+    updateAvatar,
 }
